f1c9ff511ba4d1b55fc76bc829b0ffe71ae9d63d
Gregory Kiar <gkiar@gra-login3.graham.sharcnet>
2019-01-16 23:05:05 -0500

    reduced time limit

--- code/experiments/preproc_nkirs/clowdrcmd.sh
+++ code/experiments/preproc_nkirs/clowdrcmd.sh
@@ -6,4 +6,4 @@ clowdr local descriptor.json \
              -v /project/6008063/gkiar/:/project/6008063/gkiar/ \
              -s ${PWD}/fsl_5.0.11_dwi_preprocessing-latest.simg \
              -bV \
-             -c slurm -a time:16:00:00,mem:8096,account:rpp-aevans-ab
+             -c slurm -a time:6:00:00,mem:8096,account:rpp-aevans-ab
