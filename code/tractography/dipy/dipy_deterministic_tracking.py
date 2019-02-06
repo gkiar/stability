1a55a4c7b328fc6d73cb8ce65e9f1e76b19438ff
gkiar <gkiar07@gmail.com>
2019-02-06 17:30:42 -0500

    added graph generation from streamlines; TODO:cleanitall

--- code/tractography/dipy/dipy_deterministic_tracking.py
+++ code/tractography/dipy/dipy_deterministic_tracking.py
@@ -7,20 +7,22 @@ from dipy.tracking.streamline import Streamlines
 from dipy.core.gradients import gradient_table
 from dipy.direction import peaks_from_model
 from dipy.reconst.shm import CsaOdfModel
-from dipy.io.streamline import save_trk
+from dipy.io.streamline import save_trk, load_trk
 from dipy.io import read_bvals_bvecs
 from dipy.data import default_sphere
 from dipy.tracking import utils
 from dipy.viz import have_fury
 from nibabel.streamlines import ArraySequence
 
+import matplotlib.pyplot as plt
 import nibabel as nib
 import numpy as np
+import os.path as op
 
 
 def dwi_deterministic_tracing(image, bvecs, bvals, wm, seeds, fibers,
                               prune_length=3):
-    # Pipeline trascribed from:
+    # Pipeline transcribed from:
     #   http://nipy.org/dipy/examples_built/introduction_to_basic_tracking.html
     # Load Images
     dwi_loaded = nib.load(image)
@@ -61,10 +63,8 @@ def dwi_deterministic_tracing(image, bvecs, bvals, wm, seeds, fibers,
                                  if len(strline) > prune_length])
 
     # Save streamlines
-    if not fibers.endswith(".trk"):
-        fibers += ".trk"
-    save_trk(fibers, streamlines, dwi_loaded.affine, shape=wm_data.shape,
-             vox_size=wm_loaded.header.get_zooms())
+    save_trk(fibers + ".trk", streamlines, dwi_loaded.affine,
+             shape=wm_data.shape, vox_size=wm_loaded.header.get_zooms())
 
     # Visualize fibers
     if have_fury:
@@ -82,15 +82,43 @@ def dwi_deterministic_tracing(image, bvecs, bvals, wm, seeds, fibers,
         window.record(r, n_frames=1, out_path=fibers + ".png",
                       size=(800, 800))
 
+    return streamlines, dwi_loaded.affine
+
+
+def streamlines2graph(streamlines, affine, parcellation, output_file):
+    # Load Images
+    parcellation_loaded = nib.load(parcellation)
+    parcellation_data = parcellation_loaded.get_data()
+
+    uniq = np.unique(parcellation_data)
+    parcellation_data = parcellation_data.astype(int)
+    if list(uniq) != list(np.unique(parcellation_data)):
+        raise TypeError("Parcellation labels should be integers.")
+
+    # Perform tracing
+    graph, grouping = utils.connectivity_matrix(streamlines, parcellation_data,
+                                                affine=affine,
+                                                return_mapping=True,
+                                                mapping_as_streamlines=True)
+    # Deleting edges with the background
+    graph = np.delete(graph, (0), axis=0)
+    graph = np.delete(graph, (0), axis=1)
+
+    np.savetxt(output_file + ".mat", graph)
+    plt.imshow(np.log1p(graph), interpolation='nearest')
+    plt.savefig(output_file + ".png")
+
 
 def main():
-    parser = ArgumentParser(__name__)
+    parser = ArgumentParser(__file__)
     parser.add_argument("diffusion_image")
     parser.add_argument("bvecs")
     parser.add_argument("bvals")
     parser.add_argument("whitematter_mask")
     parser.add_argument("seed_mask")
-    parser.add_argument("fiber_file")
+    parser.add_argument("output_directory")
+    parser.add_argument("--labels", "-l", nargs="+")
+    parser.add_argument("--verbose", "-v", action="store_true")
 
     results = parser.parse_args()
 
@@ -99,9 +127,30 @@ def main():
     bvals = results.bvals
     wm = results.whitematter_mask
     seeds = results.seed_mask
-    fibers = results.fiber_file
+    output = results.output_directory
+    labels = results.labels
+    verbose = results.verbose
 
-    dwi_deterministic_tracing(image, bvecs, bvals, wm, seeds, fibers)
+    bn = op.basename(image).split('.')[0]
+    fibers = op.join(output, bn + "_fibers")
+    if not op.isfile(fibers + ".trk"):
+        streamlines, affine, = dwi_deterministic_tracing(image, bvecs, bvals,
+                                                         wm, seeds, fibers)
+    else:
+        streamlines = load_trk(fibers + ".trk")
+        affine = streamlines[1]['voxel_to_rasmm']
+        streamlines = streamlines[0]
+
+    if labels:
+        graphs = []
+        for label in labels:
+            labelbn = op.basename(label).split('.')[0]
+            graphs += [op.join(output, bn + "_graph-" + labelbn)]
+            streamlines2graph(streamlines, affine, label, graphs[-1])
+
+    if verbose:
+        print("Streamlines: {0}".format(fibers))
+        print("Graphs: {0}".format(", ".join(graphs)))
 
 
 if __name__ == "__main__":
