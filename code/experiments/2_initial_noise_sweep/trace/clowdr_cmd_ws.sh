2f0f5b4eb14ea2b5801793bfc23a00aa32fc890a
gkiar <gkiar07@gmail.com>
2019-07-23 10:33:58 -0400

    initial pass at tractography running scripts

---
+++ code/experiments/2_initial_noise_sweep/trace/clowdr_cmd_ws.sh
@@ -0,0 +1,3 @@
+clowdr local oneVoxel.json invocations/ ~/data/executions/nkirs-onevoxelnoise/ --sweep mode --sweep intensity --simg ./onevoxel-v0.2.0.simg -g 10 -v /home/users/gkiar/ace_mount/ace_home/:/home/users/gkiar/ace_mount/ace_home/ -Vd
+
+clowdr local oneVoxel.json invocations/ ~/data/executions/nkirs-onevoxelnoise/ --simg ./onevoxel-v0.2.0.simg -g 10 -v /home/users/gkiar/ace_mount/ace_home/:/home/users/gkiar/ace_mount/ace_home/ -Vd --rerun incomplete --run_id 2019-02-26_12-42-51-2B7YVV6W
