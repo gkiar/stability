173230edafd28cbe85a6476aba59d2171703ea65
Greg Kiar <gkiar07@gmail.com>
2019-07-23 14:12:18 -0400

    updated descriptor and clowdr command

--- code/experiments/2_initial_noise_sweep/trace/clowdr_cmd_ws.sh
+++ code/experiments/2_initial_noise_sweep/trace/clowdr_cmd_ws.sh
@@ -1,3 +1,23 @@
-clowdr local oneVoxel.json invocations/ ~/data/executions/nkirs-onevoxelnoise/ --sweep mode --sweep intensity --simg ./onevoxel-v0.2.0.simg -g 10 -v /home/users/gkiar/ace_mount/ace_home/:/home/users/gkiar/ace_mount/ace_home/ -Vd
+clowdr local \
+       /home/users/gkiar/code/gkiar/stability/code/experiments/2_initial_noise_sweep/trace/dipy_deterministic_tracking.json \
+       invocations/ \
+       ~/data/executions/nkirs-tracking/ \
+       --simg ./dipy_deterministic_tracking-v0.1.0.simg \
+       -g 25 \
+       -v /home/users/gkiar/ace_mount/ace_home/:/home/users/gkiar/ace_mount/ace_home/ \
+       -V \
+       --cluster slurm \
+       --rerun incomplete \
+       --run_id 2019-07-23_13-57-52-GQTI22F3
 
-clowdr local oneVoxel.json invocations/ ~/data/executions/nkirs-onevoxelnoise/ --simg ./onevoxel-v0.2.0.simg -g 10 -v /home/users/gkiar/ace_mount/ace_home/:/home/users/gkiar/ace_mount/ace_home/ -Vd --rerun incomplete --run_id 2019-02-26_12-42-51-2B7YVV6W
+# clowdr local \
+#        oneVoxel.json \
+#        invocations/ \
+#        ~/data/executions/nkirs-onevoxelnoise/ \
+#        --simg ./onevoxel-v0.3.0rc3.simg \
+#        -g 50 \
+#        -v /home/users/gkiar/ace_mount/ace_home/:/home/users/gkiar/ace_mount/ace_home/ \
+#        -V \
+#        --rerun incomplete \
+#        --run_id 2019-07-23_13-47-27-XWVJZRVC\
+#        --cluster slurm
