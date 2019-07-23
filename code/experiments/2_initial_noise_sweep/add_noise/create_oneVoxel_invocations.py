2c5b1c8533fa0bad3f2f9e9306dda5f604e260d1
Greg Kiar <gkiar07@gmail.com>
2019-07-23 10:24:23 -0400

    updated noise generation scripts

--- code/experiments/2_initial_noise_sweep/add_noise/create_oneVoxel_invocations.py
+++ code/experiments/2_initial_noise_sweep/add_noise/create_oneVoxel_invocations.py
@@ -7,15 +7,28 @@ import json
 import re
 
 
-def find_files(directory):
+def find_files(directory, subj_list):
+    # Get all the diffusion images and sort them alphabetically
     diff_images = glob('{0}/sub*/ses*/dwi/*eddy.nii*'.format(directory))
     diff_images = sorted(diff_images)
 
+    # Get all the segmentations and sort them alphabetically
     mask_images = glob('{0}/sub*/ses*/dwi/*seg_2.nii*'.format(directory))
     mask_images = sorted(mask_images)
 
-    assert(len(diff_images) == len(mask_images))
+    # Create regex that will be useful for matching subject IDs
+    r = re.compile('.*/(sub-[A-Za-z0-9]+)/.*')
 
+    # Remove all values that don't match the subject-list
+    diff_images = [di for di in diff_images
+                   if r.match(di).groups()[0] in subj_list]
+    mask_images = [mi for mi in mask_images
+                   if r.match(mi).groups()[0] in subj_list]
+
+    # Assert that every chosen image has a partner
+    assert(len(diff_images) == len(mask_images) >= len(subj_list))
+
+    # And make sure that the sessions and subjects are still stored
     r = re.compile('.*/sub-([A-Za-z0-9]+)/(ses-([A-Za-z0-9]+)/)?.*')
     _ = [r.findall(d) == r.findall(m)
          for d, m in zip(diff_images, mask_images)]
@@ -35,7 +48,7 @@ def create_invocations(diff_images, mask_images, example_invocation, outdir):
 
         invoc_path = op.join(outdir, "invocation-{0}.json".format(idx))
         with open(invoc_path, 'w') as fhandle:
-            fhandle.write(json.dumps(invoc, indent=4, sort_keys=True))
+            fhandle.write(json.dumps(invoc, indent=2, sort_keys=True))
 
 
 def main():
@@ -44,13 +57,21 @@ def main():
     parser.add_argument("input_directory")
     parser.add_argument("invocation_directory")
     parser.add_argument("invocation")
+    parser.add_argument('--subjects', '-s', action="store")
     results = parser.parse_args()
 
     input_directory = results.input_directory
     invocation_directory = results.invocation_directory
     invocation = results.invocation
 
-    diffusions, wmmasks = find_files(input_directory)
+    if results.subjects:
+        with open(results.subjects) as fhandle:
+            subj_list = fhandle.read().split('\n')
+            subj_list.remove('')
+    else:
+        subj_list = None
+
+    diffusions, wmmasks = find_files(input_directory, subj_list=subj_list)
 
     create_invocations(diffusions, wmmasks, invocation, invocation_directory)
 
