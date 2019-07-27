139bd658367df359d06cb281b86b017719082bdc
Greg Kiar <gkiar07@gmail.com>
2019-07-27 19:59:51 -0400

    updated graham clowdr command

--- code/experiments/2_initial_noise_sweep/add_noise/clowdr_cmd.sh
+++ code/experiments/2_initial_noise_sweep/add_noise/clowdr_cmd.sh
@@ -6,9 +6,10 @@ clowdr local \
        ~/executions/nkirs-onevoxelnoise/ \
        --sweep mode \
        --sweep intensity \
+       --simg ./onevoxel-v0.3.0rc3.simg \
        --cluster slurm \
-       --clusterargs account:rpp-aevans-ab,time:2:00:00,mem:1024 \
-       --simg ./onevoxel-v0.2.0.simg \
-       -g 60 \
-       -V \
-       -v /project/6008063/gkiar/:/project/6008063/gkiar/ \
+       --clusterargs account:rpp-aevans-ab,time:2:00:00,mem:2048 \
+       -g 30 \
+       -dV \
+       -v /project/6008063/gkiar/:/project/6008063/gkiar/
+
