6bfde244d053b5cac23c45f3e9545f60984c9b15
Greg Kiar <gkiar07@gmail.com>
2019-07-27 20:02:04 -0400

    removed erroneous dev flag

--- code/experiments/2_initial_noise_sweep/add_noise/clowdr_cmd.sh
+++ code/experiments/2_initial_noise_sweep/add_noise/clowdr_cmd.sh
@@ -10,6 +10,6 @@ clowdr local \
        --cluster slurm \
        --clusterargs account:rpp-aevans-ab,time:2:00:00,mem:2048 \
        -g 30 \
-       -dV \
+       -V \
        -v /project/6008063/gkiar/:/project/6008063/gkiar/
 
