e678dc0de7435d1cda2d0e302da107d7ce3ffec2
gkiar <gkiar07@gmail.com>
2019-11-24 16:19:44 -0500

    added graph feature computation (for single directory at a time, with concat), and copied graph summaries

--- code/eval/graph_features.py
+++ code/eval/graph_features.py
@@ -10,19 +10,25 @@ import numpy as np
 import os
 
 
-def prep_graphs(graph_dir):
+def get_graphs(graph_dir):
     graph_files = os.listdir(op.abspath(graph_dir))
     graph_list = []
-    for graph in graph_files:
-        tmp_adj = np.loadtxt(graph)
-        graph_list += [nx.graph.Graph(tmp_adj, weighted=True, directed=False)]
-        assert tmp_adj == nx.adj_matrix(graph_list[-1]).todense()
-    return graph_files, graph_list
+    # For each graph file, open it and store the filename + graph
+    for graph_name in graph_files:
+        tmp_adj = np.loadtxt(graph_name)
+        graph_list = [{"filename": graph_name,
+                       "graph": nx.graph.Graph(tmp_adj,
+                                               weighted=True,
+                                               directed=False)}]
+        # Verify that the adjacency matrix created from the NetworkX
+        # representation matches the original adjacency matrix
+        assert tmp_adj == nx.adj_matrix(graph_list[-1]["graph"]).todense()
+    return graph_list
 
 
 def compute_summaries(graphs):
-    # Stats will be organized as follows:
-    #        name    : lambda g: function(g, *args, **kwargs)
+    # Stats organized as follows:
+    #   name : lambda/function to be called on (g)
     stats = {"edgecount": nxf.number_of_edges,
              "globaleffic": nxa.global_efficiency,
              "diameter": nxa.diameter,
@@ -37,36 +43,58 @@ def compute_summaries(graphs):
                                    for _ in t[1].values()
                                    if _ > 0]}
 
-    # create dataframe with a column per stat
-    # for each graph...
-    # add a df row for each stat on that graph
-    # add graph me
-    # for stat in stats:
+    # create dict (and eventual DF) column per stat with the following struct:
+    #  unique ID | feature1 value | feature2 value | feature3_value ....
+    feature_dicts = []
+    for graph in graphs:
+        tmp_dict = {}
+        g = graph["graph"]
+        f = op.basename(graph["filename"])
+        for stat_name in stats.keys():
+            tmp_dict["filename"] = f
+            tmp_dict["graph"] = g
+            tmp_dict[stat_name] = stats[stat_name](g)
+            tmp_dict
+        feature_dicts += [tmp_dict]
 
-    pass
-
-
-def save_stats(summaries):
-    pass
+    feature_df = pd.DataFrame.from_dict(feature_dicts)
+    return feature_df
 
 
 def concat_summaries(summary_list):
-    pass
+    dfs = []
+    for feature_df in summary_list:
+        dfs += [pd.read_hdf(feature_df, 'features')]
+
+    return pd.concat(dfs, ignore_index=True)
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("graph_dir", action="store", type=str,
+    parser.add_argument("outfile", action="store", type=str,
+                        help="Location of resulting data frame")
+    parser.add_argument("--graph_dir", "-g", action="store", nargs=1, type=str,
+                        help="Corresponding directory containing graphs with "
+                             "or without noise injected and stored in the .mat"
+                             " ASCII-encoded format. The directory structure "
+                             "expected is: graph_dir/graphs.mat")
+    parser.add_argument("--concat", "-c", action="append", nargs=1, type=str,
                         help="")
     results = parser.parse_args()
 
     if results.concat is not None:
-        concat_summaries(results.concat)
-        return 0
+        feature_df = concat_summaries(results.concat)
 
-    graph_list = prep_graphs(results.graph_dir)
-    summaries = compute_summaries(graph_list)
-    save_stats(summaries)
+    elif results.graph_dir is not None:
+        graph_list = get_graphs(results.graph_dir)
+        feature_df = compute_summaries(graph_list)
+
+    else:
+        print("Insufficient arguments provided -- specify graph dir or "
+              "summaries to concatenate. Stopping.")
+        return -1
+
+    feature_df.to_hdf(results.outfile, "features", mode='w', format="fixed")
 
 
 if __name__ == "__main__":
