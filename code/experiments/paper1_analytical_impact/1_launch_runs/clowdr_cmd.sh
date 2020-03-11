095553d2864bd1959c97c9aa18ffa79b1c54c8fa
Greg Kiar <gkiar07@gmail.com>
2020-03-11 12:30:36 -0400

    adjusting usage reqs and batching for first set of exps

--- code/experiments/paper1_analytical_impact/1_launch_runs/clowdr_cmd.sh
+++ code/experiments/paper1_analytical_impact/1_launch_runs/clowdr_cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 datapath=/project/6008063/gkiar/data/RocklandSample/derivatives/
-baseopts="-v ${datapath}:${datapath} --cluster slurm --clusterargs account:rpp-aevans-ab,time:02:00:00,mem:4096 -V --simg ~/gkiar-dipy_tracking-v0.4.0.simg --setup"
+baseopts="-v ${datapath}:${datapath} --cluster slurm --clusterargs account:rpp-aevans-ab,time:04:00:00,mem:8192 -V --simg ${HOME}/gkiar-dipy_tracking-v0.4.0.simg "
 
 #--------------------------------------------
 
@@ -12,44 +12,44 @@ python create_invocations.py ${datapath}/preproc_ds ${datapath}/connectomes_ds e
 ### Launch reference tasks
 invodir="./invocations-multi_pipeline-ref"
 clowdir="/home/gkiar/executions/nkirs/multi_pipeline/ref/"
-clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 6 --sweep "prob"
+clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 20 --sweep "prob"
 
 ### Launch simulation tasks
 invodir="./invocations-multi_pipeline"
 clowdir="/home/gkiar/executions/nkirs/multi_pipeline/mca/"
-clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 2 --sweep "prob" --sweep "output_directory"
+clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 5 --sweep "prob" --sweep "output_directory"
 
 #--------------------------------------------
 
-# Variance: Multi Seed
-### Generate invocations
-python create_invocations.py ${datapath}/preproc_ds ${datapath}/connectomes_ds example.json 10 multi_seed
-
-### Launch reference tasks
-invodir="./invocations-multi_seed-ref"
-clowdir="/home/gkiar/executions/nkirs/multi_seed/ref/"
-clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 6 --sweep "random_seed"
-
-### Launch simulation tasks
-invodir="./invocations-multi_seed"
-clowdir="/home/gkiar/executions/nkirs/multi_seed/mca/"
-clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 2 --sweep "random_seed" --sweep "output_directory"
-
-#--------------------------------------------
-
-# Signal: Age/BMI
-### Generate invocations
-python create_invocations.py ${datapath}/preproc_100 ${datapath}/connectomes example.json 20 age_bmi
-
-### Launch reference tasks
-invodir="./invocations-age_bmi-ref"
-clowdir="/home/gkiar/executions/nkirs/age_bmi/ref/"
-clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 6 --sweep "prob"
-
-### Launch simulation tasks
-invodir="./invocations-age_bmi"
-clowdir="/home/gkiar/executions/nkirs/age_bmi/mca/"
-clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 2 --sweep "prob" --sweep "output_directory"
+# # Variance: Multi Seed
+# ### Generate invocations
+# python create_invocations.py ${datapath}/preproc_ds ${datapath}/connectomes_ds example.json 10 multi_seed
+# 
+# ### Launch reference tasks
+# invodir="./invocations-multi_seed-ref"
+# clowdir="/home/gkiar/executions/nkirs/multi_seed/ref/"
+# clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 20 --sweep "random_seed"
+# 
+# ### Launch simulation tasks
+# invodir="./invocations-multi_seed"
+# clowdir="/home/gkiar/executions/nkirs/multi_seed/mca/"
+# clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 5 --sweep "random_seed" --sweep "output_directory"
 
 #--------------------------------------------
 
+# # Signal: Age/BMI
+# ### Generate invocations
+# python create_invocations.py ${datapath}/preproc_100 ${datapath}/connectomes example.json 20 age_bmi
+# 
+# ### Launch reference tasks
+# invodir="./invocations-age_bmi-ref"
+# clowdir="/home/gkiar/executions/nkirs/age_bmi/ref/"
+# clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 20 --sweep "prob"
+# 
+# ### Launch simulation tasks
+# invodir="./invocations-age_bmi"
+# clowdir="/home/gkiar/executions/nkirs/age_bmi/mca/"
+# clowdr local dipy_tracking.json ${invodir} ${clowdir} ${baseopts} -g 5 --sweep "prob" --sweep "output_directory"
+# 
+# #--------------------------------------------
+# 
